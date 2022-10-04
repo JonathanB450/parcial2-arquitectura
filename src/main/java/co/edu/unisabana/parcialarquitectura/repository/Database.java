6854d3237b99c77dde5ebae14657329c6e51cd0f
Jonathan Buitrago Roncancio <jonathanbuitragoroncancio50@gmail.com>
2022-10-04 08:26:34 -0500

    Add dependency inversion for Buy and Database class

--- src/main/java/co/edu/unisabana/parcialarquitectura/repository/Database.java
+++ src/main/java/co/edu/unisabana/parcialarquitectura/repository/Database.java
@@ -1,7 +1,9 @@
 package co.edu.unisabana.parcialarquitectura.repository;
 
-public class Database {
 
+public class Database implements IDatabase {
+
+  @Override
   public int savePurchase(int buyerCode, String item) {
     if (buyerCode == 0 || item == null || item.isEmpty()) {
       return 0;
